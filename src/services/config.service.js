f7d7ed5845d9be1786a5665860d695c4c447d580
kamin6 <aminkhushi20@gmail.com>
2021-03-30 11:38:30 +0100

    changed back-end url

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://quiz-api-KA.eu-west-2.elasticbeanstalk.com/';
+    service.apiBase = 'https://quiz-api-ajb.qaalabs.com/';
 
     return service;
 }]);
